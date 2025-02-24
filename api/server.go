38d1f0657e007d2703a3b753471c1344e1e1b28f
Toan <bachmatoan2910@gmail.com>
2025-02-24 18:26:55 -0500

    done section 2 - 19.

--- api/server.go
+++ api/server.go
@@ -23,6 +23,8 @@ func NewServer(store db.Store) *Server {
 	}
 
 	// routers down below
+	router.POST("/users", server.createUser)
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
