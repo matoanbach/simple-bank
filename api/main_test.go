4498b6f65959aaf4c43a2efa1213d38f8922db36
Toan <bachmatoan2910@gmail.com>
2025-02-24 22:11:08 -0500

    login user with JWT or PASETO

--- api/main_test.go
+++ api/main_test.go
@@ -3,10 +3,26 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	db "github.com/matoanbach/simple-bank/db/sqlc"
+	"github.com/matoanbach/simple-bank/db/util"
+	"github.com/stretchr/testify/require"
 )
+
+func NewTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetric:      util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
--- api/server.go
+++ api/server.go
@@ -1,37 +1,43 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/matoanbach/simple-bank/db/sqlc"
+	"github.com/matoanbach/simple-bank/db/util"
+	"github.com/matoanbach/simple-bank/token"
 )
 
 // Server serves HTTP requests for our banking service
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	tokenMaker token.Maker
+	store      db.Store
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetric)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	// routers down below
-	router.POST("/users", server.createUser)
-
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
-	router.POST("/transfers", server.createTransfer)
-
-	server.router = router
-	return server
+	server.setupServer()
+	return server, nil
 }
 
 // Start running the HTTP server on a specific address
@@ -41,4 +47,17 @@ func (server *Server) Start(address string) error {
 
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) setupServer() {
+	router := gin.Default()
+
+	// routers down below
+	router.POST("/users", server.createUser)
+	router.GET("/users/login", server.loginUser)
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+	router.POST("/transfers", server.createTransfer)
+	server.router = router
 }
