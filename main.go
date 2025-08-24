dcad477a2a96cc6f81034d28285dc9344b3e8ab8
Toan <bachmatoan2910@gmail.com>
2025-08-24 14:01:30 -0400

    main.go: tested running the server

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/matoanbach/simple_bank/api"
+	db "github.com/matoanbach/simple_bank/db/sqlc"
+)
+
+const (
+	DBDriver = "postgres"
+	DBSource = "postgresql://root:secret@localhost:49466/simple_bank?sslmode=disable"
+)
+
+func main() {
+	conn, err := sql.Open(DBDriver, DBSource)
+	if err != nil {
+		log.Fatal("unable to connec to DB", err)
+	}
+	store := db.NewStore(conn)
+	// runServer(store)Serve
+	server, _ := api.NewServer(store)
+	server.Serve()
+}
+
+func runServer(store *db.Store) {
+	server, err := api.NewServer(store)
+	if err != nil {
+		log.Fatal("cannot create the server")
+	}
+
+	server.Serve()
+}
